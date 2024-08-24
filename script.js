a6a104976ff7ff13706c36411bd4480c03119e0e
NoodlePy <144525667+NoodlePy@users.noreply.github.com>
2024-08-24 12:57:02 +0530

    Initial Upload v1.0
    Working Version.

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
+// Global Variables
 var turn = "X";
-var playerWin = false;
-const wrongSound = new Audio('sounds/wrong.mp3');
 const sqr_area = document.getElementsByClassName("sqr_area")[0];
 const sqr_elements = document.getElementsByClassName("sqr");
-
-window.onload = function() {
-    console.log("aa");
-}
-
 const squares = {
     a1: document.querySelector("#a1").textContent,
     a2: document.querySelector("#a2").textContent,
@@ -19,7 +13,15 @@ const squares = {
     c2: document.querySelector("#c2").textContent,
     c3: document.querySelector("#c3").textContent
 };
+let playerWin = false;
 
+// Sounds
+const tieSound = new Audio('sounds/tie.mp3');
+const winSound = new Audio('sounds/right.mp3');
+
+
+
+// Main Functions
 function getWinningCombinations() {
     return [
         ["a1", "a2", "a3"],
@@ -33,40 +35,30 @@ function getWinningCombinations() {
     ];
 }
 
-function wrongShow(player, clickedSquare) {
-    if (playerWin == false) {
-        const winningCombinations = getWinningCombinations();
-        for (let combination of winningCombinations) {
-            let playerCount = 0;
-            let emptyCount = 0;
-            for (let square of combination) {
-                if (squares[square] === player) {
-                    playerCount++;
-                } else if (squares[square] === "") {
-                    emptyCount++;
-                }
-            }
-            if (playerCount === 2 && emptyCount === 1) {
-                const squareElement = document.querySelector(`#${clickedSquare}`);
-                if (squareElement) {
-                    squareElement.classList.add("wrong_show");
-                    sqr_area.classList.add("shake");
-                    setTimeout(() => {
-                        sqr_area.classList.remove("shake");
-                    }, 500); // Remove the class after 2 seconds
-                    setTimeout(() => {
-                        squareElement.classList.remove("wrong_show");
-                    }, 500); // Remove the class after 2 seconds
-                }
-                return true;
-            }
+function checkAllFilled() {
+    const allFilled = Object.values(squares).every(value => value !== "");
+    return allFilled
+}
+
+function playWrongSound() {
+    const wrongSound = new Audio('sounds/wrong.mp3');
+    wrongSound.play();
+}
+
+
+// Win Implementation
+
+function winCheck(winningCombinations) {
+    winningCombinations.forEach((combination, index) => {
+        if (combination.every(square => squares[square] !== "" && squares[square] === squares[combination[0]])) {
+            playerWin = true;
+            winShow(index);
+            return true;
         }
-        return false;
-    }
+    });
 }
 
 function winShow(index) {
-    playerWin = true;
     const winningCombinations = getWinningCombinations();
     const winningCombination = winningCombinations[index];
 
@@ -80,25 +72,79 @@ function winShow(index) {
 
     for (var i = 0; i < sqr_elements.length; i++) {
         sqr_elements[i].classList.remove("wrong_show");
+        sqr_elements[i].classList.remove("shake");
     }
 
-    // Play the winning sound
-    const winSound = new Audio('sounds/right.mp3');
-    wrongSound.pause();
     winSound.play();
-    wrongSound.pause();
+    stopGame();
+}
+
+
+
+
+// Tie Implementation
+
+function tieCheck() {
+    if (!playerWin){
+        if (checkAllFilled()) {
+            tieShow();
+            return true;
+        }
+    }
 }
 
 function tieShow() {
     for (var i = 0; i < sqr_elements.length; i++) {
         sqr_elements[i].classList.add("tie_show");
+        sqr_elements[i].classList.remove("shake");
     }
 
     // Play the tie sound
-    const tieSound = new Audio('sounds/tie.mp3');
     tieSound.play();
+    stopGame();
 }
 
+
+// Bad Move Implementation
+
+function wrongCheck(parentArray , lastMove) {
+    console.log(lastMove)
+    let n_turn = turn == "X" ? "O" : "X";
+    console.log(n_turn);
+    for(var i = 0; i < parentArray.length; i++){
+        let emptySquares = 0; let filledSquares = 0;
+        for(var j = 0; j < parentArray[i].length; j++){
+            if (!parentArray[i][j].includes(lastMove)){
+                if (squares[parentArray[i][j]] == ""){
+                    emptySquares++;
+                } else if (squares[parentArray[i][j]] == n_turn){
+                    filledSquares++;
+                }
+            }
+            if (emptySquares == 1 && filledSquares == 2){
+                wrongShow(lastMove);
+            }
+        }
+
+    }
+}
+
+function wrongShow(lastMove) {
+    if (!playerWin){
+        const wrongMove = document.getElementById(lastMove);
+        wrongMove.classList.add("wrong_show");
+        wrongMove.classList.add("shake");
+        setTimeout(() => {
+            wrongMove.classList.remove("wrong_show");
+            wrongMove.classList.remove("shake");
+        }, 500);
+        playWrongSound();
+    }
+}
+
+
+
+// Stop Game Implementation
 
 function stopGame() {
     const sqr_elements = document.getElementsByClassName("sqr");
@@ -107,17 +153,16 @@ function stopGame() {
     }
 }
 
+
+
+// Main Event Listener
+
 for (var i = 0; i < sqr_elements.length; i++) {
     sqr_elements[i].addEventListener("click", function() {
         const pElement = this.querySelector(".sqr_text");
         if (pElement.textContent != "") {
             return;
         } else {
-            const currentPlayer = turn;
-            const clickedSquare = this.id;
-            if (wrongShow(currentPlayer, clickedSquare)) {
-                wrongSound.play();
-            }
             pElement.textContent = turn;
             if (turn == "X") {
                 turn = "O";
@@ -136,25 +181,9 @@ for (var i = 0; i < sqr_elements.length; i++) {
         squares.c1 = document.querySelector("#c1").textContent;
         squares.c2 = document.querySelector("#c2").textContent;
         squares.c3 = document.querySelector("#c3").textContent;
-
-        const winningCombinations = getWinningCombinations();
-
-        let isWinner = false;
-        winningCombinations.forEach((combination, index) => {
-            if (combination.every(square => squares[square] !== "" && squares[square] === squares[combination[0]])) {
-                stopGame();
-                winShow(index);
-                isWinner = true;
-                return;
-            }
-        });
-
-        if (!isWinner) {
-            const allFilled = Object.values(squares).every(value => value !== "");
-            if (allFilled) {
-                stopGame();
-                tieShow();
-            }
-        }
+        if (!winCheck(getWinningCombinations())){
+            wrongCheck(getWinningCombinations() , pElement.parentElement.id);
+            tieCheck();
+        };
     });
 }
